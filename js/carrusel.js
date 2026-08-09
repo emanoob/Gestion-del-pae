130effca0eddf99d5ca75f357d2a379868c7ff3b
EMANUEL <emanuelgomezch8@gmail.com>
2026-08-09 11:58:08 -0500

    Añadi el carrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,8 +1,19 @@
 const grande=document.querySelector('.grande')
-const punto=document.querySelector('.punto')
+const punto=document.querySelectorAll('.punto')
+
 
 punto.forEach((cadaPunto,i) => {
     punto[i].addEventListener("click", () =>{
-        
+        let position=i;
+        let porcentaje=100/punto.length
+        let operacion=i* -porcentaje;
+        grande.style.transform ='translateX('+operacion+'%)'
+
+        punto.forEach(( cadaPunto, i)=>{
+            punto[i].classList.remove('activo')
+        })
+        punto[i].classList.add('activo')
     })
+
+    
 });
