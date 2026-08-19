17b8f3b07a9888b73b0b4256e5700adcde87b955
Emanoob <emanuelgomezch8@gmail.com>
2026-08-19 15:40:30 -0500

    App principal base

---
+++ html/app/app.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion del PAE</title>
+    <link rel="stylesheet" href="../../css/estilo.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Anton&family=Monda:wght@400..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+    </style>
+
+
+    
+    
+</head>
+<body>
+    
+    <header>
+
+    <section class="superior">
+
+        <img src="../../img/icon.png" alt="Logo" class="logo">
+
+        <h1>GESTIÓN ALIMENTARIA</h1>
+
+        <form class="buscador" id="buscadorForm">
+            <input type="search" id="busqueda" placeholder="Buscar...">
+            <button type="submit">🔎</button>
+        </form>
+
+    </section>
+    <button id="abrir" class="menu">☰</button>
+    <nav class="nav" id="nav">
+
+        
+
+
+        <img src="../../img/casa.png" alt="Inicio" class="casa">
+
+        <ul class="despl-list">
+            <li><a href="#" >Inicio</a></li>
+
+            <li>
+                <a href="../rutentrega.html" >
+                    Rutas de Entrega
+                </a>
+            </li>
+
+            <li><a href="../servicioCliente.html">Atención y servicios</a></li>
+
+            <li><a href="../pagina4.html">Acceder al servicio</a></li>
+
+            <li><a href="../iniciarSesion.html">Iniciar sesión</a></li>
+        </ul>
+
+    </nav>
+
+</header>
+    <main>
+        <section class="options_app">
+            <button><img alt="boton" src=""></button>
+            <ul class="option_app_item">
+                <img alt="option_item_img" src="">
+                <li>Añadir</li>
+            </ul>
+            <ul class="option_app_item">
+                <img alt="option_item_img" src="">
+                <li>Ver registros</li>
+            </ul>
+            <ul class="option_app_item">
+                <img alt="option_item_img" src="">
+                <li>Graficar</li>
+            </ul>
+            <ul class="option_app_item">
+                <img alt="option_item_img" src="">
+                <li>Reportar</li>
+            </ul>
+        </section>
+        <section class="app_forms">
+
+        </section>
+    </main>
+
+    <footer>
+
+    <section class="redes">
+
+        <p>
+            <a href="https://www.instagram.com/gestion_alimenticia_jorge?utm_source=qr&igsh=ZWJ4ZDU0bTM3bDIy">
+            <img src="../../img/redes sociales/lnstagram.png">
+            gestion_alimenticia_Medellin
+            </a>
+        </p>
+
+        <p>
+            <a href="https://wa.me/573107398094?text=Hola%20quiero%20más%20información%20sobre%20sus%20servicios" target="_blank">
+            <img src="../../img/redes sociales/w.png">
+            3234724735
+            </a>
+        </p>
+
+        <p>
+            <a href="https://www.facebook.com/share/1JHe1Jqxjx/">
+                <img src="../../img/redes sociales/f.png">
+                @gestion_alimenticia-med
+            </a>
+        </p>
+
+    </section>
+
+    <section class="redes">
+
+        <p>
+            <a href=" https://www.tiktok.com/@gomezyeral2323._?_r=1&_t=ZS-96wEMCMsZ7P">
+            <img src="../../img/tito.png">
+            @Gestion_alimenticia_
+            </a>
+        </p>
+
+    </section>
+
+    <section class="alcaldia">
+
+        <img src="../../img/logo-med.png" alt="Alcaldía de Medellín">
+
+        <p>Alcaldía de Medellín</p>
+
+    </section>
+
+    </footer>
+    <script src="js/mHam.js"></script>
+    <script src="js/carrusel.js"></script>
+    <script src="js/buscador.js"></script>
+</body>
+</html>
