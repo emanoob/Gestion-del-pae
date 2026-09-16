0382669ccca252039709399f6586a1a95e6b9791
Emanoob <emanuelgomezch8@gmail.com>
2026-09-16 15:57:38 -0500

    te puede registrar

--- base_datos/informes/conexion/abrir_conexion.php
+++ base_datos/informes/conexion/abrir_conexion.php
@@ -7,13 +7,9 @@
 
     $tabla_db1="informes";
 
-<<<<<<< HEAD
-    $conexion = new mysqli($host, $usuariodb,$clavedb,$basededatos);
-=======
     $conexion = new mysqli($host, $usuariodb,$clavedb,$basededatos,);
     
      /*desde aqui y el 3307 lo borro cuando lo cierro
->>>>>>> ff04e86eb28cd16111c8a524be97ff9d95270339
     if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
